d6406be3602f274befe60409b875cb00881e2eb5
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-09-24 23:19:43 +0200

    add component Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Profile } from './Profile/Profile';
 import user from './Profile/user.json';
 
-
+import { Statistics } from './Statistics/Statistics';
+import data from './Statistics/data.json';
 
 export const App = () => {
   return (
     <main>
       <Profile user={user} />
+      <Statistics title="Upload stats" stats={data} />
     </main>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+import { StatisticsList } from './StatisticsList';
+import css from './Statistics.module.css';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>{title}</h2>
+      <StatisticsList stats={stats} />
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  stats: PropTypes.array.isRequired,
+};
