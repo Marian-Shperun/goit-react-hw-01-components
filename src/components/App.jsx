5039c2927d4f974a1301cd0ffa8ced7a45c37562
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-09-24 23:17:35 +0200

    add component Prolife

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
+import React from 'react';
+import { Profile } from './Profile/Profile';
+import user from './Profile/user.json';
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main>
+      <Profile user={user} />
+    </main>
   );
 };
