25637d844219428821c479a1e0df5385bf48dfe1
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-09-25 13:59:00 +0200

    add component transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,19 @@ import user from './Profile/user.json';
 import { Statistics } from './Statistics/Statistics';
 import data from './Statistics/data.json';
 
+import { FriendList } from './FriendList/FriendList';
+import friends from './FriendList/friends.json';
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
+
 export const App = () => {
   return (
     <main>
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </main>
   );
 };
