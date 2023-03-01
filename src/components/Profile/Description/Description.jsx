5cf890ec35307ea67e964e7e799b5d44c9d4f05c
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2023-03-01 12:04:39 +0100

    Update Description.jsx

--- src/components/Profile/Description/Description.jsx
+++ src/components/Profile/Description/Description.jsx
@@ -4,7 +4,7 @@ const Description = ({ username, tag, location, avatar }) => {
   return (
     <div className="description">
       <img src={avatar} alt={username} className={css.avatar} />
-      <p className={css.name}>{username}</p>
+      <p className={css.name}>{username}.</p>
       <p className="tag">{tag}</p>
       <p className="location">{location}</p>
     </div>
