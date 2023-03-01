9eec611369809871a3ead6d92acef9ad75fa8fa8
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2023-03-01 12:26:45 +0100

    adds slider grid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,5 @@
-import React from 'react';
-import { Profile } from './Profile/Profile';
-import user from './Profile/user.json';
+import Slider from "./Slider";
 
-import { Statistics } from './Statistics/Statistics';
-import data from './Statistics/data.json';
-
-import { FriendList } from './FriendList/FriendList';
-import friends from './FriendList/friends.json';
-
-import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from './TransactionHistory/transactions.json';
-const { username, tag, location, avatar, stats } = user;
 export const App = () => {
-  return (
-    <main>
-      <Profile
-        username={username}
-        tag={tag}
-        location={location}
-        avatar={avatar}
-        stats={stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-    </main>
-  );
+  return <Slider />; 
 };
